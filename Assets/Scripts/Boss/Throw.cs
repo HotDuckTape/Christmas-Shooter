09eb03cf2036c3e8d8edfd4ec85109452be453a1
HotDuckTape <89712057+HotDuckTape@users.noreply.github.com>
2023-12-11 19:51:03 +0100

    Throw Worked On

--- Assets/Scripts/Boss/Throw.cs
+++ Assets/Scripts/Boss/Throw.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Throw : MonoBehaviour
 {
-    private GameObject player;
-    [SerializeField] private float minTimer;
     [SerializeField] private float maxTimer;
+    [SerializeField] private float minTimer;
+    [SerializeField] private GameObject objectToThrow;
+    [SerializeField] private Transform spawnPos;
+    private GameObject player;
+    public AnimationCurve throwCurve;
+    public float throwDuration = 2f;
     private float timer;
 
     private void Start()
@@ -24,7 +28,28 @@ public class Throw : MonoBehaviour
         if (timer <= 0)
         {
             Debug.Log("Ur Mom");
+            StartCoroutine(ThrowCoroutine());
             timer = Random.Range(minTimer, maxTimer);
         }
+    }
+
+    private IEnumerator ThrowCoroutine()
+    {
+        GameObject cupcake = Instantiate(objectToThrow, spawnPos.position, spawnPos.rotation);
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < throwDuration)
+        {
+            float normalizedTime = elapsedTime / throwDuration;
+            float curveValue = throwCurve.Evaluate(normalizedTime);
+            Vector3 throwPosition = Vector3.Lerp(cupcake.transform.position, player.transform.position, normalizedTime);
+            throwPosition += Vector3.up * curveValue;
+            cupcake.transform.position = throwPosition;
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+        //Damage
     }
 }
