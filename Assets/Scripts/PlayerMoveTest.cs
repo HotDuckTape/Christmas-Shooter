67d52c0a54ee22ecf2e60513ab2061aefe84d7d8
Obayouh <80900668+Obayouh@users.noreply.github.com>
2023-12-13 14:44:19 +0100

    Standard Animations and Temp Player Prefab added

---
+++ Assets/Scripts/PlayerMoveTest.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMoveTest : MonoBehaviour
+{
+
+    //PlayerInput playerInput;
+
+    //InputAction moveAction;
+    //[SerializeField] private float moveSpeed = 4f;
+    //void Start()
+    //{
+    //    playerInput = GetComponent<PlayerInput>();
+    //    moveAction = playerInput.actions.FindAction("Move");
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    MovePlayer();
+    //}
+
+    //private void MovePlayer()
+    //{
+    //    Vector2 direction = moveAction.ReadValue<Vector2>();
+    //    transform.position += new Vector3(direction.x, 0, direction.y) * moveSpeed * Time.deltaTime;
+    //}
+}
